9e9da1efa6492dee85b2e15cb7e9c5a8fb25ac1a
dgjisi <92454690+dgjisi@users.noreply.github.com>
2024-08-31 16:47:33 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.jisicn.cf"];
-pub const PUBLIC_RS_PUB_KEY: &str = "YcoJN1ykkjpPhI7SotjJCgYnxMTq6CyeIu06Ouc77GU=";
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
